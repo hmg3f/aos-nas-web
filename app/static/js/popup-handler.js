f0c602738008ff8614418304d51dba4c8fefbd84
hmg3f <hmg3f@umsystem.edu>
2025-11-15 20:22:19 -0600

    update file viewer stylesheet

--- app/static/js/popup-handler.js
+++ app/static/js/popup-handler.js
@@ -1,4 +1,5 @@
 const popups = document.querySelectorAll('.popup, .prompt');
+const panels = document.querySelectorAll('.panel');
 
 popups.forEach(popup => {
     const h1 = popup.querySelector('h1');
@@ -7,6 +8,22 @@ popups.forEach(popup => {
 
 	if (event.target === h1) {
 	    popup.style.display = 'none';
+	}
+    });
+});
+
+
+panels.forEach(panel => {
+    const h1 = panel.querySelector('h1');
+    const content = panel.querySelector('.content');
+
+    h1.addEventListener('click', function(event) {
+	if (event.target === h1) {
+	    if (content.style.display === 'none') {
+		content.style.display = 'flex';
+	    } else {
+		content.style.display = 'none';
+	    }
 	}
     });
 });
