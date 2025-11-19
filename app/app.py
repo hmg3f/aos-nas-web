048a1c550435b5d0a7d1d43b43b6c177208beee0
hmg3f <hmg3f@umsystem.edu>
2025-11-19 13:23:27 -0600

    increase file upload size limit

--- app/app.py
+++ app/app.py
@@ -19,7 +19,7 @@ app.register_blueprint(auth, url_prefix='/auth')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(DATABASE_PATH, 'nasinfo.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'os3N95B6Z9cs'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 * 1024
 
 db.init_app(app)
 
